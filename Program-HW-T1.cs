0697f3f066474cd8007d92b49d32183fefe8426b
MichaelVerh <107642304+MichaelVerh@users.noreply.github.com>
2022-07-17 21:43:55 +0300

    Add files via upload

---
+++ Program-HW-T1.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+Console.WriteLine("Введите число");
+string Number=Console.ReadLine();
+if (Number.Length>2)
+{
+    Console.WriteLine($"Третья цифра числа {Number} - это {Number[2]}");
+}
+else
+{
+    Console.WriteLine($"В числе {Number} менее трех цифр");
+}
